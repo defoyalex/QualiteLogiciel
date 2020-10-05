c62f3f4775759d49855c62f189d82590ca781b27
VindictiV415 <71785508+VindictiV415@users.noreply.github.com>
2020-10-04 20:28:51 -0400

    updated LOC counter et import counter

--- TP1/Main.java
+++ TP1/Main.java
@@ -27,7 +27,7 @@ public class Main {
                 ArrayList<File> newJavaFiles = getListJavaFiles(path + file.getName() + "/");
                 if (newJavaFiles != null) {
                     newJavaFiles.addAll(allJavaFiles);
-                    allJavaFiles = newJavaFiles; //DÉCOMMENTER SI ON VEUT PASSER TOUS LES DOSSIERS RÉCURSIVEMENT
+                    //allJavaFiles = newJavaFiles; //DÉCOMMENTER SI ON VEUT PASSER TOUS LES DOSSIERS RÉCURSIVEMENT
                 }
             }
         }
@@ -38,7 +38,7 @@ public class Main {
     // Lis une liste de fichiers 0
     public static ArrayList<ClasseMetriques> readFiles(ArrayList<File> files) {
         ArrayList<ClasseMetriques> classeMetriques = new ArrayList<ClasseMetriques>();
-
+        int importLineCounter = 0;
         var metriques = new Metriques(); //utilisé pour compter les lignes de javadoc précédant déclaration de classes
 
         for (int i = 0; i < files.size(); i++) {
@@ -51,6 +51,10 @@ public class Main {
                     String line = reader.nextLine();
 
                     metriques.javadocLineCounter(line);
+
+                    if (metriques.isImportLines(line)) { //si c'est une déclaration de "import ..."
+                        importLineCounter++;
+                    }
 
                     if (isClass(line)) {
                         //TODO si la ligne est le début d'une classe
@@ -66,7 +70,7 @@ public class Main {
                         }
                         int currentJavadocLinesNumber = metriques.getJavadocLineCounter();
                         metriques.resetJavadocLineCounter(); //on réinitialise le compteur de ligne de javadoc
-                        ClasseMetriques nouvelleClasse = new ClasseMetriques(files.get(i).toString(), line, currentJavadocLinesNumber);
+                        ClasseMetriques nouvelleClasse = new ClasseMetriques(files.get(i).toString(), line, currentJavadocLinesNumber + importLineCounter);
                         classeMetriques.add(nouvelleClasse);
                     }
                 }
@@ -116,8 +120,8 @@ public class Main {
 
     public static void main(String[] args) {
         //String folder = "./classesTest/jfree/chart";
-        //String folder = "./";
-        String folder = "E:/Documents/GitHub/QualiteLogiciel/TP1/classesTest/jfree/";
+        String folder = "E:/Documents/GitHub/QualiteLogiciel/TP1/";
+        //String folder = "E:/Documents/GitHub/QualiteLogiciel/TP1/classesTest/jfree/";
         //String folder = "E:/Documents/GitHub/QualiteLogiciel/TP1/classesTest/jfree/chart/TESTING";
         ArrayList<File> listFiles = getListJavaFiles(folder);
 
